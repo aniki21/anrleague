b4b57737af390bdbefaafabb3db37cbb78adf2f2
Paul C <pacosgrove1@googlemail.com>
2016-05-14 23:32:58 +0100

    use correct base url in production emails; fix crash for nil description_html

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer
-  config.action_mailer.default_url_options = { host: "anrleague.herokuapps.com" }
+  config.action_mailer.default_url_options = { host: "anrleague.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
